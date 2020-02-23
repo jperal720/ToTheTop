e454f019780f49b901eedffe5dee7b8ef780e5d2
brownbuddyguy <61366283+brownbuddyguy@users.noreply.github.com>
2020-02-22 20:38:33 -0800

    param is fixing bullet

---
+++ ToTheTop/Assets/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+    float moveSpeed = 7f;
+
+    Rigidbody2D rb;
+    public Transform target;
+    Vector2 moveDirection;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.Equals("Player"))
+        {
+            Debug.Log("Hit");
+            Destroy(gameObject);
+        }
+    }
+}
