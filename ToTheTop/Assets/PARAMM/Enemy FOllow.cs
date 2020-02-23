820b36139043f970f41e039a03d0e55f03c1c84f
brownbuddyguy <61366283+brownbuddyguy@users.noreply.github.com>
2020-02-22 19:17:03 -0800

    enemy movment paramm
    ladder chases bridge if spots it in certain distance

--- ToTheTop/Assets/PARAMM/Enemy FOllow.cs
+++ ToTheTop/Assets/PARAMM/Enemy FOllow.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class EnemyFOllow : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public float speed;
+    private Transform target;
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(transform.position, target.position) > 3)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
     }
 }
---
+++ ToTheTop/Assets/enemymovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemymovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed;
+    public Transform target;
+    public float radius;
+    private bool check = false;
+    public float enemydistance;
+
+    // Update is called once per frame
+    void Update()
+    {   if (Vector2.Distance(transform.position, target.position) < radius && check==false) {
+            check = true;
+        }
+
+        if (Vector2.Distance(transform.position, target.position) > enemydistance && check==true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+    }
+}
